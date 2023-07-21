396d640daee60750e89409731c6cdd35294d4bbd
Jonas <sanoj.camargo@gmail.com>
2023-07-21 02:54:53 -0300

    funcção to_grid() rece a string, grid geral e a posição da palavra no grid. essa função retorna um objeto com dados sobre a palavra (tamanho, index da letra(s) que foi encontrada no grid, quantas celulas (se houver) vai espandir o grid) e testa se a palavra realmente encaixa no grid

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
-let grid = []
-let vertical = false
-
-
 // ***** retorna numero passado por parametro sempre como positivo
 const to_positive = ( num ) => ( num < 0 ) ? num * -1 : num
+
+// ***** retorna uma nova array com os itens da mesma index como uma coluna da matriz
+Array.prototype.get_column = function( index ){ return this.map( row => row[index] ) }
 
 // ***** retorna uma nova array com os itens da mesma index como uma coluna da matriz
 Array.prototype.col_content = function( index ){ return this.map( row => row[index] ) }
@@ -11,6 +10,25 @@ Array.prototype.col_content = function( index ){ return this.map( row => row[ind
 // ***** procura e retorna a index da(s) linha(s) que contem as letras da nova palavra
 Array.prototype.in_rows = function( string )
 {
+    let result  = []
+    let letters = string.split('')
+
+    this.find(( row, i ) => {
+        let ltr_counter = row.filter( ltr => letters.includes(ltr) ).length
+        let word_size = string.length
+        
+        let row_up = this[i -1]
+        let row_bm = this[i +1]
+
+        if( ltr_counter > 0 ){
+            console.log( `⬆️ ${row_up}` )
+            console.log( `linha ${row}` )
+            console.log( `⬇️ ${row_bm}` )
+            console.log( '-------------' )
+        }
+    })
+    
+    /*
     let letters = string.split('')
     let grid_size = this.length
     let result  = []
@@ -24,8 +42,8 @@ Array.prototype.in_rows = function( string )
     })
 
     return result
+    */
 }
-
 // ***** procura e retorna a index da(s) coluna(s) que contem as letras da nova palavra
 Array.prototype.in_cols = function( string )
 {
@@ -34,7 +52,7 @@ Array.prototype.in_cols = function( string )
 
     for( let i = 0; i < this[0].length; i++ )
     {
-        let column  = this.col_content(i)
+        let column  = this.get_column(i)
         let counter = column.filter( (char) => letters.includes(char) ).length
 
         if( counter !== 0 ) result.push( i )
@@ -43,328 +61,157 @@ Array.prototype.in_cols = function( string )
     return result
 }
 
-// **** aumentar tamanho da matriz
-// recebe uma array de dois valores [ x"horizontal", y"vertical" ]
-// valores negativo para esquerda, positivo para direita!
-Array.prototype.growth = function( up_xy ){
-    if( (to_positive(up_xy[0]) + to_positive(up_xy[1])) === 0 ) return
-
-    let x_num = up_xy[0]
-    let y_num = up_xy[1]
-
-    // horizontal
-    if( x_num !== 0 )
-    {
-        let up = Array( to_positive(x_num) ).fill(0)
-
-        if( x_num < 0 )
-            this.map(( lin ) => lin.unshift(...up) )
-        else
-            this.map(( lin ) => lin.push(...up) )
-    }
-
-    // vertical
-    if( y_num !== 0 )
-    {
-        let grid_size = this[0].length
-        let up = Array.from({ length: to_positive(y_num) }, () => Array(grid_size + to_positive(x_num)).fill(0))
-
-        if( y_num < 0 )
-            this.unshift(...up)
-        else
-            this.push(...up)
-    }
-}
-
-
-
-////////////////////
-
-grid = [
-    ['Z', 0, 0, 0],
-    [0, 0, 0, 'P'],
-    ['Z', 0, 0, 0],
-    ['X', 0, 0, 0],
-]
-
-console.table( grid )
-console.warn( '-----' )
-
-//////////
-
-add_word = function( word ){
+String.prototype.to_grid = function( way, arr, pos ){
     let valid = true
-    let rows  = ( vertical ) ? grid.in_cols( word ) : grid.in_rows( word )
-    
-    let letters = word.split('')
-    
+    let row   = pos[0]
+    let col   = pos[1]
+    let grid  = arr
 
-    for( let i = 0; i < rows.length; i++ )
-    {
-        console.group( ((vertical) ? 'VERTICAL' : 'HORIZONTAL') +' - '+ word )
-
-        let content = ( vertical ) ? grid.col_content(rows[i]) : grid[rows[i]]
-        let cntt_lft_top = ( vertical ) ? grid.col_content(rows[i] -1) : grid[rows[i] -1]
-        let cntt_rgt_btm = ( vertical ) ? grid.col_content(rows[i] +1) : grid[rows[i] +1]
-
-        // letra em comum *na nova palavra e na grid (coluna ou linha)*
-        let same_ltr = letters.find((x) => {
-            return content.find((y) => { return (y === x) ? y : 0 })
-        })
-
-        // index da letra em comum NA PALAVRA
-        let index_letter = letters.indexOf( same_ltr )
-        // index da letra em comum NA GRID
-        let index_grid   = content.indexOf( same_ltr )
-
-        // colunas extra para atualizar o grid
-        // valor negativo aumenta para esquernda - valor positivo aumenta para direita
-        let grid_update = index_grid - index_letter
-        if( vertical && (grid_update >= 0 ) ) {
-            grid_update = letters.length - (content.length - index_grid)
-        }
-        // index na linha onde começa a palavra
-        let index_start = ( grid_update < 0 ) ? 0 : grid_update
-
-        if( grid_update !== 0 ){
-            // ***** VERTICAL
-            if( vertical )
-                grid.growth([ 0, grid_update ])
-            // ***** HORIZONTAL
-            else
-                grid.growth([ grid_update, 0 ])
-        }
-
-        console.error( grid_update )
-        console.error( index_start )
-        for( let c = 0; c < letters.length; c++ )
-        {
-            // ***** VERTICAL
-            if( vertical ){
-                // grid[index_start +c][rows[i]] = letters[c]
-                console.log( index_start +c )
-            }
-            // ***** HORIZONTAL
-            else {
-                content[index_grid +c] = letters[c]
-            }
-        }
-
-        console.table( grid )
-
-        console.groupEnd()
-        vertical = !vertical
+    let word  = {
+        size: this.length,
+        letters: this.split(''),
+        found: []
     }
+
+    console.log(word.letters)
+
+    // *** guarda todas as letras da string encontradas no grid e suas indexs (na string e no grid)
+    word.letters.map(( ltr, i ) => {
+        let gx = grid[row].indexOf( ltr ) // index do caracter encontrado no grid
+
+        // [ LETRA, INDEX NA STRING, INDEX NA MATRIZ ]
+        if( gx >= 0 ) word.found.push( [ ltr, i, gx ] )
+    })
+    
+    // *** se alguma letra foi encontrada valida a linha ou coluna
+    // verifica se o inicio ou o fim do espaço espaço onde a palavra ficará no grid esta livre
+    // verifica se a parte de cima/baixo (ou direita/esquerda) estão livres
+    if( word.found.length > 0 && valid ){
+        console.log('...1')
+        // primeira letra encontrada
+        let c_  = word.found.filter((i) => typeof i !== undefined).shift() // primeira letra encontrada
+        let c_s = word.size
+        let c_x = c_[1]
+        let c_up = ( c_s - c_x -1 )
+
+        let g_x = c_[2] // index no grid
+        let g_s = arr[row].length // tamanho do grid
+
+        let c_i = ( (c_x - g_x) < 0 ) ? (g_x - c_x) : 0 // posição da primeira letra no grid
+        let c_f = ( (c_up + g_x) > g_s ) ? g_s : (c_up + g_x) // posição da ultima letra no grid
+
+        let valid_before = ((c_x - g_x) < 0) ? 0 : (c_x - g_x)
+        let valid_after  = ((c_x - c_s) * -1) - ((g_x - g_s) * -1)
+            valid_after  = (valid_after < 0) ? 0 : valid_after
+
+        word['before'] = valid_before
+        word['after']  = valid_after
+
+        if(
+            (arr[row][c_i -1] !== undefined && arr[row][c_i -1] !== 0) ||
+            (arr[row][c_f +1] !== undefined && arr[row][c_f +1] !== 0)
+        ){
+            console.log('...2')
+            valid = false
+        }
+
+        let loop = g_x +1
+
+        for( let i = loop; i < g_s; i++ ){
+            console.log('...3')
+            let g_char = arr[row][i]
+            let c_char = word.letters[i -1]
+
+            let cont_valid = ( g_char === 0 ) ? true : ((g_char === c_char) ? 1 : 0)
+
+            console.log(`
+            i [${i}]
+            cont_valid [${cont_valid}]
+            (${word.letters[c_[1] +1]})
+            ---
+            g_char [${g_char}]
+            g_char === 0 [${g_char === 0}]
+            ---
+            g_char [${g_char}]
+            c_char [${c_char}]
+            g_char === c_char [${g_char === c_char}]
+            `)
+            
+            if( !cont_valid ){
+                console.log('...4')
+                // valid = false
+                // break
+            }
+        }
+    }
+
+    console.log( word )
+
+    if( valid ) return word
 }
 
-add_word( 'PATY' )
-// add_word( 'NDUBFFY' )
-add_word( 'YNDUBFF' )
-
-////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let grid = []
 let vertical = false
 
-
-// ***** retorna numero passado por parametro sempre como positivo
-const to_positive = ( num ) => ( num < 0 ) ? num * -1 : num
-
-// ***** retorna uma nova array com os itens da mesma index como uma coluna da matriz
-Array.prototype.col_content = function( index ){ return this.map( row => row[index] ) }
-
-// ***** procura e retorna a index da(s) linha(s) que contem as letras da nova palavra
-Array.prototype.in_rows = function( string )
-{
-    let letters = string.split('')
-    let grid_size = this.length
-    let result  = []
-
-    this.filter(( lin, i ) => {
-        let counter = lin.filter( ltr => letters.includes(ltr) ).length
-        let error   = lin.filter( char => char !== 0 && !letters.includes(char) )
-        let free_space = lin.filter( char => char === 0 ).length;
-
-        if( counter !== 0 && free_space > counter ) result.push( i )
-    })
-
-    return result
-}
-
-// ***** procura e retorna a index da(s) coluna(s) que contem as letras da nova palavra
-Array.prototype.in_cols = function( string )
-{
-    let letters = string.split('')
-    let result  = []
-
-    for( let i = 0; i < this[0].length; i++ )
-    {
-        let column  = this.col_content(i)
-        let counter = column.filter( (char) => letters.includes(char) ).length
-
-        if( counter !== 0 ) result.push( i )
-    }
-
-    return result
-}
-
-// **** aumentar tamanho da matriz
-// recebe uma array de dois valores [ x"horizontal", y"vertical" ]
-// valores negativo para esquerda, positivo para direita!
-Array.prototype.growth = function( up_xy ){
-    if( (to_positive(up_xy[0]) + to_positive(up_xy[1])) === 0 ) return
-
-    let x_num = up_xy[0]
-    let y_num = up_xy[1]
-
-    // horizontal
-    if( x_num !== 0 )
-    {
-        let up = Array( to_positive(x_num) ).fill(0)
-
-        if( x_num < 0 )
-            this.map(( lin ) => lin.unshift(...up) )
-        else
-            this.map(( lin ) => lin.push(...up) )
-    }
-
-    // vertical
-    if( y_num !== 0 )
-    {
-        let grid_size = this[0].length
-        let up = Array.from({ length: to_positive(y_num) }, () => Array(grid_size + to_positive(x_num)).fill(0))
-
-        if( y_num < 0 )
-            this.unshift(...up)
-        else
-            this.push(...up)
-    }
-}
-
-
-
-////////////////////
-
-grid = [
-    ['Z', 0, 0, 0],
-    [0, 0, 0, 'P'],
-    ['Z', 0, 'Y', 0],
-    ['X', 0, 0, 0],
+let game_board = [
+    [0, 'N', 'M', 'C'],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 'X', 0, 0],
+    [0, 'P', 0, 0],
 ]
 
-console.table( grid )
-console.warn( '-----' )
+console.table( game_board )
+console.log( 'PUABNMCAMM'.to_grid( vertical, game_board, [0,2] ) )
 
-//////////
+/////////////////////////////
 
-add_word = function( word ){
+
+
+function addWord( word )
+{
     let valid = true
-    let rows  = ( vertical ) ? grid.in_cols( word ) : grid.in_rows( word )
+    let vertical = false
+
+    let grid = game_board
+    // guarda a index da linha(row) ou coluna(col) onde a nova palavra pode ficar
+    let cont_index = ( vertical ) ? grid.in_cols( word ) : grid.in_rows( word )
+
+    let word_arr  = word.split('')
+    let word_size = word_arr.length
+    let word_hook = 0
+
+    console.log( cont_index )
+
+    game_board.map(() => {
+
+        console.log(`
+        • palavra na [${(vertical)?'vertical':'horizontal'}]
+        `)
     
-    let letters = word.split('')
+        /*
+        words.some(( word ) => {
+            const word = {
+                site:    string.length,
+                letters: string.split(''),
+                hook:    char => string.indexOf( char )
+            }
     
-
-    for( let i = 0; i < rows.length; i++ )
-    {
-        console.group( ((vertical) ? 'VERTICAL' : 'HORIZONTAL') +' - '+ word )
-        // console.log( rows )
-
-        let content = ( vertical ) ? grid.col_content(rows[i]) : grid[rows[i]]
-        let cntt_lft_top = ( vertical ) ? grid.col_content(rows[i] -1) : grid[rows[i] -1]
-        let cntt_rgt_btm = ( vertical ) ? grid.col_content(rows[i] +1) : grid[rows[i] +1]
-
-        console.log( content )
-
-        // ***** VERTICAL
-        if( vertical ){}
-        // ***** HORIZONTAL
-        else {}
-
-        let same_ltr = letters.find((x) => {
-            return content.find((y) => { return (y === x) ? y : 0 })
+            const board = {
+                grid:   game_board,
+                target: ( vertical ) ? grid.get_column(rows[i]) : grid[rows[i]]
+            }
+    
+    
+            console.log( word.hook('A') )
+    
+            vertical = !vertical
         })
-
-        let index_ltr = letters.indexOf( same_ltr )
-        let index_row = content.indexOf( same_ltr )
-
-        let grid_hor_update   = index_row - index_ltr // valor negativo aumenta a grid para esquernda, valor positivo aumenta a grid para direita
-        let start_word_inline = index_row - index_ltr // index na linha onde começa a palavra
-        start_word_inline = ( start_word_inline < 0 ) ? 0 : start_word_inline
-
-        console.log( `letra [${same_ltr}] index na string: [${index_ltr}] - index na ${( vertical )? 'coluna':'linha'}: [${index_row}] - mais [${grid_hor_update}] celulas` )
-        console.log( `inde do inicio da palavra na linha: [${start_word_inline}]` )
-
-        if( grid_hor_update !== 0 ){
-            // ***** VERTICAL
-            if( vertical ){
-                grid.growth( [0, grid_hor_update] )
-            }
-            // ***** HORIZONTAL
-            else {
-                grid.growth( [grid_hor_update, 0] )
-            }
-        }
-
-        for( let c = 0; c < letters.length; c++ )
-        {
-            // ***** VERTICAL
-            if( vertical ){
-                // console.log( grid[rows[i]][start_word_inline +c] )
-                // console.log( `grid[${rows[i]}][${start_word_inline +c}]` )
-                // console.log( `grid[${start_word_inline +c}][${rows[i]}]` )
-                // grid[rows[i]][start_word_inline +c] = '_'
-                // grid[start_word_inline +c][rows[i]] = '_'
-                let y = rows[i]
-                let x = start_word_inline +c
-                
-                
-            }
-            // ***** HORIZONTAL
-            else {
-                console.log( '---' )
-                content[start_word_inline +c] = letters[c]
-            }
-        }
-
-        console.table( grid )
-
-
-        console.groupEnd()
+        */
+        
         vertical = !vertical
-    }
+    })
 }
 
-add_word( 'PATO' )
-add_word( 'YNDUMM' )
+// addWord( 'PATO' )
+// addWord( 'PORTA' )
 
-
-////////////////////
-
-*/
