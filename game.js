43c7f36dc3bc1e8a47cc254618ada98de29cda21
Jonas <sanoj.camargo@gmail.com>
2023-07-26 12:41:07 -0300

    atualizando o codigo

--- game.js
+++ game.js
@@ -9,20 +9,35 @@ class Crossword
 }
 
 // *** adicionando nova palavra no array
-Crossword.prototype.addWord = function( string ){
+Crossword.prototype.wordAdd = function( string ){
     console.log(`adicionando a palavra [${string}]`)
 
     // se for a primeira palavra evita o loop
-    if( this.grid.length === 0 ) this.grid.push( this.vertical ? string.split('').map( ltr  => [ ltr ]) : string.split('') )
+    if( this.grid.length === 0 ){
+        this.grid.push( this.vertical ? string.split('').map( ltr  => [ ltr ]) : string.split('') )
+        return
+    }
 
     // passar pelo grid até encontrar a letra em comum com a palavra
-    this.grid.some(( ltr ) => {
-        console.log( ltr )
-    })
+    let gridFound = this.gridFinder( string )
+    console.log( gridFound )
+
+    let rows = gridFound.map((i) => i.rowIndex)
+    let cols = gridFound.map((i) => i.colIndex)
+    console.log( cols )
+
+    // arr.filter((item, index) => array.indexOf(item) === index)
 
     // encontrar a letra em comum
+    // console.log( this.grid )
+}
 
-    console.log( this.grid )
+// GRID
+// *** procura a palavra na grid - retorna a letra e index da linha e coluna [ letter:'A', rowIndex:0, colIndex:0 ]
+Crossword.prototype.gridFinder = function( string ){
+    return this.grid.flatMap((row, rowIndex) =>
+        row.map((letter, colIndex) => ({ letter, rowIndex, colIndex }))
+    ).filter(item => string.includes(item.letter))
 }
 
 // *** atualiza o tamanho da array
@@ -38,5 +53,5 @@ console.log( game )
 
 
 // adicionando palavra
-game.addWord( 'PATO' )
-game.addWord( 'PLUMA' )
+game.wordAdd( 'PATO' )
+game.wordAdd( 'PLUMA' )
